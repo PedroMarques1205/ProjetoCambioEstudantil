0c7203577de5a926e986aa50e286d343decd772b
pedro <pedrophmo1205@gmail.com>
2024-10-23 17:02:54 -0300

    ConÇertakkkkk escrevi com ç, eu gosto dessa letra

--- Codigo/backProjetoMoeda/src/main/java/com/pucminas/backprojetomoeda/model/TrocaMoedas.java
+++ Codigo/backProjetoMoeda/src/main/java/com/pucminas/backprojetomoeda/model/TrocaMoedas.java
@@ -1,13 +1,24 @@
 package com.pucminas.backprojetomoeda.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity(name = "TrocaMoedas")
 public class TrocaMoedas {
 
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long idTroca;
 
+    @ManyToOne
+    @JoinColumn(name = "consumidor_email")
     Usuario consumidor;
 
     Double valorMoedasGastas;
 
+    @ManyToOne
+    @JoinColumn(name = "vantagem_adquirida_id")
     Vantagem vantagemAdquirida;
 
 }
--- Codigo/backProjetoMoeda/src/main/java/com/pucminas/backprojetomoeda/model/Vantagem.java
+++ Codigo/backProjetoMoeda/src/main/java/com/pucminas/backprojetomoeda/model/Vantagem.java
@@ -1,4 +1,22 @@
 package com.pucminas.backprojetomoeda.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity(name = "Vantagens")
 public class Vantagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String titulo;
+
+    String descricao;
+
+    Double valor;
+
+    @ManyToOne
+    @JoinColumn(name = "empresa_provedor_email")
+    Usuario empresaProvedor;
 }
